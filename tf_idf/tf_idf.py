5929af706f7142186147a83daa6b9b0f8cc3c0e8
Artem Konyshev <konyshev.ar@gmail.com>
2020-10-25 20:54:05 +0300

    - implement tf_idf calculation - refactor

--- tf_idf/tf_idf.py
+++ tf_idf/tf_idf.py
@@ -1,7 +1,10 @@
 import os
+import math
 
 from mrjob.job import MRJob
 from mrjob.step import MRStep
+
+DOC_COUNT = 0
 
 
 def get_file_name():
@@ -14,11 +17,12 @@ class DocWordFrequencyStep(MRStep):
         super().__init__(**kwargs, mapper=self.mapper, reducer=self.reducer)
 
     def mapper(self, _, line):
+        doc = get_file_name()
         for word in line.split():
-            yield (get_file_name(), word), 1
+            yield (doc, word), 1
 
-    def reducer(self, doc_word, amounts):
-        yield doc_word, sum(amounts)
+    def reducer(self, doc_word, amounts_generator):
+        yield doc_word, sum(amounts_generator)
 
 
 class DocWordCountStep(MRStep):
@@ -26,23 +30,77 @@ class DocWordCountStep(MRStep):
     def __init__(self, **kwargs):
         super().__init__(**kwargs, mapper=self.mapper, reducer=self.reducer)
 
-    def mapper(self, doc_word, amount):
-        yield doc_word[0], (doc_word[1], amount)
+    def mapper(self, key, amount):
+        doc = key[0]
+        word = key[1]
+        yield doc, (word, amount)
 
-    def reducer(self, doc, words_counts):
-        words_counts = list(words_counts)
-        doc_words_count = sum(word_count[1] for word_count in words_counts)
-        for word_count in words_counts:
-            yield (word_count[0], doc), (word_count[1], doc_words_count)
+    def reducer(self, doc, values_generator):
+        values_list = list(values_generator)
+        doc_words_count = sum(word_count[1] for word_count in values_list)
+        for values in values_list:
+            word = values[0]
+            word_count = values[1]
+            yield (word, doc), word_count / doc_words_count
 
 
-class TFIDF(MRJob):
+class CorpusWordFrequencyStep(MRStep):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs, mapper=self.mapper, reducer=self.reducer)
+
+    def mapper(self, key, tf):
+        word = key[0]
+        doc = key[1]
+        yield word, (doc, tf, 1)
+
+    def reducer(self, word, values_generator):
+        values_list = list(values_generator)
+        corpus_word_count = sum(values[2] for values in values_list)
+        for values in values_list:
+            doc = values[0]
+            tf = values[1]
+            yield (word, doc), (tf, corpus_word_count)
+
+
+class TFIDFStep(MRStep):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs, mapper=self.mapper, reducer=self.reducer)
+
+    def mapper(self, word_doc, values):
+        global DOC_COUNT
+        tf = values[0]
+        corpus_word_count = values[1]
+        idf = math.log(DOC_COUNT / corpus_word_count)
+        yield word_doc, tf * idf
+
+    def reducer(self, word_doc, tfidf_generator):
+        for tfidf in tfidf_generator:
+            yield word_doc, tfidf
+
+
+class TFIDFJob(MRJob):
+
+    def __init__(self, args=None):
+        self.doc_count = 0
+        super().__init__(args)
+
+    def run(self):
+        global DOC_COUNT
+        DOC_COUNT = len(self.options.args)
+        super().run()
+
+    def get_doc_count(self):
+        return self.doc_count
+
     def steps(self):
         return [
             DocWordFrequencyStep(),
-            DocWordCountStep()
+            DocWordCountStep(),
+            CorpusWordFrequencyStep(),
+            TFIDFStep()
         ]
 
 
 if __name__ == '__main__':
-    TFIDF().run()
+    TFIDFJob().run()

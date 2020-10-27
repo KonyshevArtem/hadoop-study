b736ab30a7c89a15e4c76b626f2930fca34459ad
Artem Konyshev <konyshev.ar@gmail.com>
2020-10-27 19:35:19 +0300

    - add doc count step instead of global variable - add more word delimiters

--- tf_idf/tf_idf.py
+++ tf_idf/tf_idf.py
@@ -5,8 +5,7 @@ import re
 from mrjob.job import MRJob
 from mrjob.step import MRStep
 
-DOC_COUNT = 0
-WORD_DELIMITERS = r'[ ,.()?!:;\t\n\[\]]'
+WORD_DELIMITERS = r'[ ,.()?!:;\t\n\[\]=\\\/]'
 WORD_BLACKLIST = ['a', 'the', 'an', 'is']
 
 
@@ -14,19 +13,38 @@ def get_file_name():
     return os.path.basename(os.environ['mapreduce_map_input_file'])
 
 
-class DocWordFrequencyStep(MRStep):
+class DocCountStep(MRStep):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs, mapper=self.mapper, reducer=self.reducer)
 
     def mapper(self, _, line):
         doc = get_file_name()
+        yield 1, (doc, line)
+
+    def reducer(self, _, values_generator):
+        values_list = list(values_generator)
+        doc_count = len(set(values[0] for values in values_list))
+        for values in values_list:
+            doc = values[0]
+            line = values[1]
+            yield (doc, doc_count), line
+
+
+class DocWordFrequencyStep(MRStep):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs, mapper=self.mapper, reducer=self.reducer)
+
+    def mapper(self, key, line):
+        doc = key[0]
+        doc_count = key[1]
         for word in re.split(WORD_DELIMITERS, line):
             if len(word) > 0 and word.lower() not in WORD_BLACKLIST:
-                yield (doc, word), 1
+                yield (doc, word, doc_count), 1
 
-    def reducer(self, doc_word, amounts_generator):
-        yield doc_word, sum(amounts_generator)
+    def reducer(self, key, amounts_generator):
+        yield key, sum(amounts_generator)
 
 
 class DocWordCountStep(MRStep):
@@ -37,15 +55,18 @@ class DocWordCountStep(MRStep):
     def mapper(self, key, amount):
         doc = key[0]
         word = key[1]
-        yield doc, (word, amount)
+        doc_count = key[2]
+        yield (doc, doc_count), (word, amount)
 
-    def reducer(self, doc, values_generator):
+    def reducer(self, key, values_generator):
+        doc = key[0]
+        doc_count = key[1]
         values_list = list(values_generator)
         doc_words_count = sum(word_count[1] for word_count in values_list)
         for values in values_list:
             word = values[0]
             word_count = values[1]
-            yield (word, doc), word_count / doc_words_count
+            yield (word, doc, doc_count), word_count / doc_words_count
 
 
 class CorpusWordFrequencyStep(MRStep):
@@ -56,26 +77,32 @@ class CorpusWordFrequencyStep(MRStep):
     def mapper(self, key, tf):
         word = key[0]
         doc = key[1]
-        yield word, (doc, tf, 1)
+        doc_count = key[2]
+        yield (word, doc_count), (doc, tf, 1)
 
-    def reducer(self, word, values_generator):
+    def reducer(self, key, values_generator):
+        word = key[0]
+        doc_count = key[1]
         values_list = list(values_generator)
         corpus_word_count = sum(values[2] for values in values_list)
         for values in values_list:
             doc = values[0]
             tf = values[1]
-            yield (word, doc), (tf, corpus_word_count)
+            yield (word, doc, doc_count), (tf, corpus_word_count)
 
 
 class TFIDFStep(MRStep):
     def __init__(self, **kwargs):
         super().__init__(**kwargs, mapper=self.mapper, reducer=self.reducer)
 
-    def mapper(self, word_doc, values):
+    def mapper(self, key, values):
+        word = key[0]
+        doc = key[1]
+        doc_count = key[2]
         tf = values[0]
         corpus_word_count = values[1]
-        idf = math.log(DOC_COUNT / corpus_word_count)
-        yield word_doc, tf * idf
+        idf = math.log(doc_count / corpus_word_count)
+        yield (word, doc), tf * idf
 
     def reducer(self, word_doc, tfidf_generator):
         for tfidf in tfidf_generator:
@@ -84,13 +111,12 @@ class TFIDFStep(MRStep):
 
 class TFIDFJob(MRJob):
 
-    def run(self):
-        global DOC_COUNT
-        DOC_COUNT = len(self.options.args)
-        super().run()
+    def __init__(self, args=None):
+        super().__init__(args)
 
     def steps(self):
         return [
+            DocCountStep(),
             DocWordFrequencyStep(),
             DocWordCountStep(),
             CorpusWordFrequencyStep(),
